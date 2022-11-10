9621219f83a5a707836c3c178d3c748a3520891d
mrinnnmoy <mrinmoy202000@gmail.com>
2022-11-10 08:44:06 +0530

    add header-section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Toggle from "../img/toggle-arrow.svg";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome</h1>
+    <div className="home">
+
+      <div className="container">
+        <div className="header">
+          <img src={Toggle} alt="arrow" />
+          <h3>Trending Assets</h3>
+        </div>
+      </div>
     </div>
   )
 }
