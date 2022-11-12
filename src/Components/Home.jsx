f5d60d8f6f623ee661f9c8612bf756e31740b066
mrinnnmoy <mrinmoy202000@gmail.com>
2022-11-12 19:03:26 +0530

    build home-section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,18 +1,116 @@
-import React from 'react';
-import Toggle from "../img/toggle-arrow.svg";
+import React from "react";
+// import Toggle from "../img/toggle-arrow.svg";
 
 const Home = () => {
   return (
     <div className="home">
 
-      <div className="container">
-        <div className="header">
-          <img src={Toggle} alt="Toggle-button" className="toggle-img" />
-          <h3>Trending Assets</h3>
+      <div className="header">
+        {/* Icon */}
+        <p className="header-title">Trending Assets</p>
+      </div>
+
+      {/* Assets */}
+      <div className="tokens">
+
+        {/* Asset-1 */}
+        <div className="token-1">
+          <img src="" alt="bitcoin" className="token-img" />
+          <p className="token-name">Bitcoin (BTC)</p>
+          <div className="token-price">
+            <p className="current-price">$31,812.80</p>
+            <p className="change-price">+30%</p>
+          </div>
+          <p>Price</p>
+          <input type="number" className="token-amount" />
+          <p>TVL</p>
+          <div className="pair-img">
+            <img src="" alt="sol" className="pair-1" />
+            <img src="" alt="eth" className="pair-2" />
+            <img src="" alt="binance" className="pair-3" />
+          </div>
+          <p>Popular pairs</p>
         </div>
+
+        {/* Asset-2 */}
+        <div className="token-2">
+          <img src="" alt="solana" className="token-img" />
+          <p className="token-name">Solana (SOL)</p>
+          <div className="token-price">
+            <p className="current-price">$32.83</p>
+            <p className="change-price">-12.32%</p>
+          </div>
+          <p>Price</p>
+          <input type="number" className="token-amount" />
+          <p>TVL</p>
+          <div className="pair-img">
+            <img src="" alt="btc" className="pair-1" />
+            <img src="" alt="eth" className="pair-2" />
+            <img src="" alt="binance" className="pair-3" />
+          </div>
+          <p>Popular pairs</p>
+        </div>
+
+        {/* Asset-3 */}
+        <div className="token-3">
+          <img src="" alt="Eth" className="token-img" />
+          <p className="token-name">ETHEREUM (ETH)</p>
+          <div className="token-price">
+            <p className="current-price">$1466.45</p>
+            <p className="change-price">-11.93%</p>
+          </div>
+          <p>Price</p>
+          <input type="number" className="token-amount" />
+          <p>TVL</p>
+          <div className="pair-img">
+            <img src="" alt="sol" className="pair-1" />
+            <img src="" alt="btc" className="pair-2" />
+            <img src="" alt="binance" className="pair-3" />
+          </div>
+          <p>Popular pairs</p>
+        </div>
+
+        {/* Asset-4 */}
+        <div className="token-4">
+          <img src="" alt="BUSD" className="token-img" />
+          <p className="token-name">Binance USD (BUSD)</p>
+          <div className="token-price">
+            <p className="current-price">$1.00</p>
+            <p className="change-price">+0.26%</p>
+          </div>
+          <p>Price</p>
+          <input type="number" className="token-amount" />
+          <p>TVL</p>
+          <div className="pair-img">
+            <img src="" alt="sol" className="pair-1" />
+            <img src="" alt="eth" className="pair-2" />
+            <img src="" alt="binance" className="pair-3" />
+          </div>
+          <p>Popular pairs</p>
+        </div>
+
+        {/* Asset-5 */}
+        <div className="token-5">
+          <img src="" alt="SHIB" className="token-img" />
+          <p className="token-name">Shiba Inu (SHIB)</p>
+          <div className="token-price">
+            <p className="current-price">$0.00000001948</p>
+            <p className="change-price">-0.1%</p>
+          </div>
+          <p>Price</p>
+          <input type="number" className="token-amount" />
+          <p>TVL</p>
+          <div className="pair-img">
+            <img src="" alt="sol" className="pair-1" />
+            <img src="" alt="eth" className="pair-2" />
+            <img src="" alt="binance" className="pair-3" />
+          </div>
+          <p>Popular pairs</p>
+        </div>
+
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
